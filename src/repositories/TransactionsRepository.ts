a0ad156636a884678d08ee4b32837e213088ca04
Carlos Krueger <crlalexandrek@gmail.com>
2020-07-12 01:12:39 -0300

    feat: challenge completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,22 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const balance: Balance = transactions.reduce(
+      (acc: Balance, value: Transaction) => {
+        if (value.type === 'income') acc.income += Number(value.value);
+        else acc.outcome += Number(value.value);
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    balance.total = balance.income - balance.outcome;
+    return balance;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,55 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const categoriesRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('Invalid transaction type');
+    }
+
+    const { total } = await transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new AppError('There is not enough balance for this transaction.');
+    }
+
+    let trasactionCategory = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!trasactionCategory) {
+      trasactionCategory = categoriesRepository.create({ title: category });
+      await categoriesRepository.save(trasactionCategory);
+    }
+
+    const newTransaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: trasactionCategory,
+    });
+
+    await transactionsRepository.save(newTransaction);
+    return newTransaction;
   }
 }
 
